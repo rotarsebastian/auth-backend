d045e0c18228878f0b8a895b9199aa7542ff973a
Sebastian Rotar <rotar.seby1@gmail.com>
2020-05-06 15:17:43 +0200

    configs

--- knexfile.js
+++ knexfile.js
@@ -14,12 +14,13 @@ module.exports = {
   },
 
   prooduction: {
-    client: 'mysql',
-    connection: {
-      host: process.env.DB_HOST || '',
-      user: process.env.DB_USER || username,
-      password: process.env.DB_USER_PASS || password,
-      database: process.env.DB || database,
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: __dirname + '/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/seeds'
     }
   },
 
