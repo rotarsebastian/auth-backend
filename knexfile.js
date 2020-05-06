23c07dbbbc1f48f71482e495ecbaf42bf99490ae
Sebastian Rotar <rotar.seby1@gmail.com>
2020-05-06 15:07:39 +0200

    configs

--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,17 @@ const { knexSnakeCaseMappers } = require('objection');
 module.exports = {
 
   development: {
-    client: require('knex')({ client: 'mysql' }),
+    client: 'mysql',
+    connection: {
+      host: process.env.DB_HOST || '',
+      user: process.env.DB_USER || username,
+      password: process.env.DB_USER_PASS || password,
+      database: process.env.DB || database,
+    }
+  },
+
+  prooduction: {
+    client: 'mysql',
     connection: {
       host: process.env.DB_HOST || '',
       user: process.env.DB_USER || username,
